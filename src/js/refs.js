87e838031d4f4fb0715b85ae5f2178bf32c489d3
Liza Hannoshyna <fox8pro8@gmail.com>
2025-09-23 18:00:27 +0300

    modal

--- src/js/refs.js
+++ src/js/refs.js
@@ -5,7 +5,22 @@ const refs = {
     dropdown: document.getElementById("countryDropdown"),
     countryList: document.getElementById("countryList"),
     selectedCountry: document.getElementById("selectedCountry"),
-    dropdownArrow: document.getElementById("dropdownArrow")
+    dropdownArrow: document.getElementById("dropdownArrow"),
+    
+    modal: document.getElementById('modal-window'),
+    closeBtn: document.getElementById('modal-window_close-icon'),
+    modalTopPhoto: document.getElementById('modal-window_top-photo'),
+    modalMainPhoto: document.getElementById('modal-winow_main-photo'),
+    infoDescr: document.getElementById('info-descr'),
+    whenDescrDate: document.getElementById('when-descr_date'),
+    whenDescrTime: document.getElementById('when-descr_time'),
+    whereDescrCity: document.getElementById('where-descr_city'),
+    whereDescrPlace: document.getElementById('where-descr_place'),
+    whoDescrAuthor: document.getElementById('who-descr_author'),
+    descrStandartPrice: document.getElementById('descr_standart-price'),
+    descrVipPrice: document.getElementById('descr_vip-price'),
+    buyStandartBtn:document.getElementById('buyStandartBtn'),
+    buyVipBtn:document.getElementById('buyVipBtn')
 }
 
 export default refs;
