bda098e76d98b3deee4a6ba07abbefa49da217e3
Liza Hannoshyna <fox8pro8@gmail.com>
2025-09-24 14:51:51 +0300

    animation

--- src/index.js
+++ src/index.js
@@ -3,3 +3,6 @@ import './js/posters.js'
 import './js/modal.js';
 import './js/pagination.js';
 import './js/search.js'
+
+import { initPreloader } from './js/preloader.js';
+initPreloader();
--- src/js/refs.js
+++ src/js/refs.js
@@ -6,21 +6,25 @@ const refs = {
     countryList: document.getElementById("countryList"),
     selectedCountry: document.getElementById("selectedCountry"),
     dropdownArrow: document.getElementById("dropdownArrow"),
-    
+
     modal: document.getElementById('modal-window'),
-    closeBtn: document.getElementById('modal-window_close-icon'),
-    modalTopPhoto: document.getElementById('modal-window_top-photo'),
-    modalMainPhoto: document.getElementById('modal-winow_main-photo'),
-    infoDescr: document.getElementById('info-descr'),
-    whenDescrDate: document.getElementById('when-descr_date'),
-    whenDescrTime: document.getElementById('when-descr_time'),
-    whereDescrCity: document.getElementById('where-descr_city'),
-    whereDescrPlace: document.getElementById('where-descr_place'),
-    whoDescrAuthor: document.getElementById('who-descr_author'),
-    descrStandartPrice: document.getElementById('descr_standart-price'),
-    descrVipPrice: document.getElementById('descr_vip-price'),
-    buyStandartBtn:document.getElementById('buyStandartBtn'),
-    buyVipBtn:document.getElementById('buyVipBtn')
+  closeBtn: document.getElementById('modal-window_close-icon'),
+  modalTopPhoto: document.getElementById('modal-window_top-photo'),
+  modalMainPhoto: document.getElementById('modal-winow_main-photo'),
+  infoDescr: document.getElementById('info-descr'),
+  whenDescrDate: document.getElementById('when-descr_date'),
+  whenDescrTime: document.getElementById('when-descr_time'),
+  whereDescrCity: document.getElementById('where-descr_city'),
+  whereDescrPlace: document.getElementById('where-descr_place'),
+  whoDescrAuthor: document.getElementById('who-descr_author'),
+  descrStandartPrice: document.getElementById('descr_standart-price'),
+  descrVipPrice: document.getElementById('descr_vip-price'),
+  buyStandartBtn: document.getElementById('buyStandartBtn'),
+  buyVipBtn: document.getElementById('buyVipBtn'),
+
+
+  moreFromAuthorBtn: document.getElementById('author-more-btn'),
+  authorEvents: document.getElementById('authorEvents')
 }
 
 export default refs;
